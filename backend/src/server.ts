a67300a8d630b308ffb526db87ccf90c143727ec
Henrik Lievonen <henrik.lievonen@hotmail.com>
2018-04-06 20:26:38 +0300

    Move to use vue.js
    React did not please us.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,13 +3,14 @@ import {createExpressServer} from "routing-controllers";
 import {RegistrationController} from "./controllers/RegistrationController";
 import express = require("express");
 import proxy = require("express-http-proxy");
+import path = require("path");
 
 
 const app = createExpressServer({
     controllers: [RegistrationController]
 });
 
-app.use("/", proxy("http://localhost:3000/"));
+app.use("/", express.static(path.resolve(__dirname, "./frontend/")));
 
 console.log("Running");
 app.listen(8000);
