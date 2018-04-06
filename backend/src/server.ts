d32da459c56f198818b415ef8179864803776439
Henrik Lievonen <henrik.lievonen@hotmail.com>
2018-04-07 00:03:00 +0300

    Base for new backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,54 @@
 import "reflect-metadata";
-import {createExpressServer} from "routing-controllers";
+import { createExpressServer, useContainer as routingUseContainer} from "routing-controllers";
 import {RegistrationController} from "./controllers/RegistrationController";
 import express = require("express");
-import proxy = require("express-http-proxy");
 import path = require("path");
+import fs = require("fs");
+import { createConnection, useContainer as ormUseContainer, Repository } from "typeorm";
+import { Form, Field, Restriction, FilledField, FilledForm } from "./models";
+import { Container, Service } from "typedi";
+import { InjectRepository } from "typeorm-typedi-extensions";
+import { DevDatabaseCreator } from "./devDatabaseCreator";
+import { WSASYSNOTREADY } from "constants";
 
+async function main() {
+    try{
+        routingUseContainer(Container);
+        ormUseContainer(Container);
+        let dbConfig = JSON.parse(fs.readFileSync("../config/db.config.json", "utf8"));
+        let connection = await createConnection({
+            type: dbConfig.type,
+            host: dbConfig.host,
+            port: dbConfig.port,
+            username: dbConfig.username,
+            password: dbConfig.password,
+            database: dbConfig.database,
+            entities: [
+                Form,
+                Field,
+                Restriction,
+                FilledForm,
+                FilledField
+            ],
+            synchronize: true,
+            logging: true
+        });
 
-const app = createExpressServer({
-    controllers: [RegistrationController]
-});
+        console.log("Connected to database");
+        await Container.get(DevDatabaseCreator).create();
+        console.log("Development database created");
 
-app.use("/", express.static(path.resolve(__dirname, "./frontend/")));
+        const app = createExpressServer({
+            controllers: [RegistrationController]
+        });
 
-console.log("Running");
-app.listen(8000);
+        app.use("/", express.static(path.resolve(__dirname, "./frontend/")));
+
+        console.log("Running");
+        app.listen(8000);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+main();
