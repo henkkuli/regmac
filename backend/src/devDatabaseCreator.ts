23800c492850dd8d80e6c030ab7229150ffdf4d4
Henrik Lievonen <henrik.lievonen@hotmail.com>
2018-04-20 19:45:39 +0300

    Second test form

--- backend/src/devDatabaseCreator.ts
+++ backend/src/devDatabaseCreator.ts
@@ -9,16 +9,28 @@ export class DevDatabaseCreator {
     private manager: EntityManager;
 
     public async create(): Promise<void> {
-        const field1 = new Field("Field 1");
-        const field2 = new Field("Field 2");
+        const field11 = new Field("Field 1");
+        const field12 = new Field("Field 2");
         const form1 = new Form("Form 1");
-        form1.fields = [field1, field2];
+        form1.fields = [field11, field12];
         await Promise.all([
-            this.manager.save(field1),
-            this.manager.save(field2),
+            this.manager.save(field11),
+            this.manager.save(field12),
         ]);
         await Promise.all([
             this.manager.save(form1),
+        ]);
+        
+        const field21 = new Field("Field 3");
+        const field22 = new Field("Field 4");
+        const form2 = new Form("Form 2");
+        form2.fields = [field21, field22];
+        await Promise.all([
+            this.manager.save(field21),
+            this.manager.save(field22),
+        ]);
+        await Promise.all([
+            this.manager.save(form2),
         ]);
     }
 }
